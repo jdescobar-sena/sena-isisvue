5ef8b536c3c8a68b175f651b018e1b7482a72ec0
jdarias <jdescobar502@misena.edu.co>
2023-08-08 21:20:09 -0500

    Implemented functionality of new account form via api consumtion

--- src/components/NewAccForm.vue
+++ src/components/NewAccForm.vue
@@ -1,86 +1,130 @@
 <template>
 	<div class="w3-container w3-center w3-padding-24">
 		<h2>Crea tu cuenta para hacer pedidos</h2>
-                <form action="SVIsisRegister" method="post" target="_self">
-                    <!-- El form va aquí -->
-                    
-                    <label for="txtFirstName">
-                        Tus nombres: 
-                        <input type="text" id="txtFirstName" name="firstName">
-                    </label>
-                    
-                    <br>
-                    
-                    <label for="txtLastName">
-                        Tus apellidos: 
-                        <input type="text" id="txtLastName" name="lastName">
-                    </label>
-                    
-                    <br>
-                    
-                    <label for="nmbDocumentId">
-                        Tu cédula: 
-                        <input type="number" id="nmbDocumentId" name="documentId">
-                    </label>
-                    
-                    <br>
-                    
-                    
-                    <label for="dateBirthdate">
-                        Tu fecha de nacimiento: 
-                        <input type="date" id="dateBirthdate" name="birthdate">
-                    </label>
-                    
-                    
-                    
-                    <br>
-                    
-                    <label for="txtAddress">
-                        Tu dirección de entrega: 
-                        <input type="text" id="txtAddress" name="address">
-                    </label>
-                    
-                    <br>
-                    
-                    <label for="emlEmail">
-                        Tu correo electrónico: 
-                        <input type="email" id="emlEmail" name="email">
-                    </label>
-                    
-                    <br>
-                    
-                    <!-- esto requiere validación de formulario
-                    <label for="emlEmailConfirm">
-                        Confirma tu correo electrónico: 
-                        <input type="email" id="emlEmailConfirm" name="emailConfirm">
-                    </label>
-                    -->
-                    
-                    <br>
-                    
-                    <label for="pwdPassword">
-                        Contraseña: 
-                        <input type="password" id="pwdPassword" name="password">
-                    </label>
-                    
-                    <br>
-                    
-                    <!-- esto requiere validación de formulario-->
-                    <label for="pwdPasswordConfirm">
-                        Confirma tu contraseña: 
-                        <input type="password" id="pwdPasswordConfirm" name="passwordConfirm">
-                    </label>
-                                        
-                    <br>
-                    
-                    <input type="submit" value="Crear mi cuenta">
-                
-                </form>
+		<form id="account-creator" @submit.prevent="saveacc">
+			<!-- El form va aquí -->
+			
+			<label for="first_name">
+				Tus nombres: 
+				<input type="text" id="first_name" name="first_name" v-model="first_name" />
+			</label>
+			
+			<br>
+			
+			<label for="last_name">
+				Tus apellidos: 
+				<input type="text" id="last_name" name="last_name" v-model="last_name" />
+			</label>
+			
+			<br>
+			
+			<label for="document_id">
+				Tu cédula: 
+				<input type="number" id="document_id" name="document_id" v-model="document_id" />
+			</label>
+			
+			<br>
+			
+			
+			<label for="birthdate">
+				Tu fecha de nacimiento: 
+				<input type="date" id="birthdate" name="birthdate" v-model="birthdate" />
+			</label>
+			
+			
+			
+			<br>
+			
+			<label for="address">
+				Tu dirección de entrega: 
+				<input type="text" id="address" name="address" v-model="address" />
+			</label>
+			
+			<br>
+			
+			<label for="email">
+				Tu correo electrónico: 
+				<input type="email" id="email" name="email" v-model="email" />
+			</label>
+			
+			<br>
+			
+			<!-- esto requiere validación de formulario
+			<label for="emlEmailConfirm">
+				Confirma tu correo electrónico: 
+				<input type="email" id="emlEmailConfirm" name="emailConfirm">
+			</label>
+			-->
+			
+			<br>
+			
+			<label for="password">
+				Contraseña: 
+				<input type="password" id="password" name="password" v-model="password" />
+			</label>
+			
+			<br>
+			
+			<!-- esto requiere validación de formulario
+			<label for="pwdPasswordConfirm">
+				Confirma tu contraseña: 
+				<input type="password" id="pwdPasswordConfirm" name="passwordConfirm">
+			</label>
+			-->
+			<br>
+			
+			<button type="submit" id="saveacc" name="saveacc">Crear mi cuenta</button>
+		
+		</form>
 	</div>
 </template>
 
 <script>
+// importar axios
+import axios from "axios";
 export default {
-  name: 'NewAccForm'
-}
+	// el nombre del componente
+	name: 'NewAccForm',
+	// los datos
+	data() {
+		return {
+			document_id: "",
+			first_name: "",
+			last_name: "",
+			birthdate: "",
+			email: "",
+			password: "",
+			address: "",
+		};
+	},
+
+	// los metodos a manejar con axios
+	methods: {
+		saveacc() {
+			axios.post("http://localhost:8080/api/accounts", {
+				document_id: this.document_id,
+				first_name:  this.first_name, 
+				last_name: this.last_name,
+				birthdate: this.birthdate,
+				email: this.email,
+				password: this.password,
+				address: this.address,
+			})
+			.then((response) => {
+				console.log("account successfully created: ", response.data);
+				alert("At_id: this.document_id, account created");
+				this.document_id = ''; 
+				this.first_name = ''; 
+				this.last_name = '';
+				this.birthdate = '';
+				this.email = '';
+				this.password = '';
+				this.address = '';
+			})
+			.catch((error) => {
+				console.error("error when creating account: ", error);
+			});
+		}
+	}
+};
 </script>
